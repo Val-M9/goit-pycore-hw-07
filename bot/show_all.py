2829a2d893535e320365e433d3f3049996c112c3
Val_M <v.mischenko90@gmail.com>
2024-07-29 13:26:41 +0300

    homework done

---
+++ bot/show_all.py
@@ -0,0 +1,7 @@
+from classes import AddressBook
+
+
+def show_all(book: AddressBook) -> str | AddressBook:
+  if not book:
+    return 'No records'
+  return book
---
+++ main.py
@@ -0,0 +1,50 @@
+from bot import (
+    add_contact,
+    show_all,
+    show_phone,
+    change_contact,
+    parse_input,
+    add_birthday,
+    show_birthday,
+    upcoming_birthdays,
+    all_commands,
+    exit_commands,
+)
+from classes import AddressBook
+
+
+def main():
+  book = AddressBook()
+  print('Welcome to assistance bot!')
+
+  while True:
+    user_input = input('Enter a command: ')
+    command, *args = parse_input(user_input)
+
+    match command:
+      case _ if command in exit_commands:
+        print('Good bye!')
+        break
+      case 'hello':
+        print('How can I help you?')
+      case 'add':
+        print(add_contact(args, book))
+      case 'all':
+        print(show_all(book))
+      case 'phone':
+        print(show_phone(args, book))
+      case 'change':
+        print(change_contact(args, book))
+      case 'add-birthday':
+        print(add_birthday(args, book))
+      case 'show-birthday':
+        print(show_birthday(args, book))
+      case 'birthdays':
+        print(upcoming_birthdays(book))
+      case _:
+        print(f'Invalid command. Here is the list of available commands: {
+              all_commands}')
+
+
+if __name__ == '__main__':
+  main()
